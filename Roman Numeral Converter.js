979f6a9be0d1f02dcbb174ae444350d960a2e26c
Recelis <jackymlui@live.com>
2017-03-08 23:40:53 +1000

    repeat problem in removedZeros

--- Roman Numeral Converter.js
+++ Roman Numeral Converter.js
@@ -4,11 +4,15 @@ var result = [];
 var ii = 0;
 function convertToRoman(num){
     var remainder = -1;
+    var maxRemainder = 1;
+     var newNum = num;
     while (remainder < 0){
+        if (Math.floor(num/divider[ii]) > 0) maxRemainder = Math.floor(num/divider[ii]);
         remainder = num - divider[ii]; // loop through divider until gives remainder
         console.log("");
         console.log("num" + num);
         console.log("divider" + divider[ii]);
+        console.log("maxRemainder " + maxRemainder);
         console.log("remainder " + remainder);
         console.log("odd or even "+ ii%2);
         if (ii%2 == 1 && remainder == 4){ // is odd so it ends with 5 and remainder is 4
@@ -20,27 +24,30 @@ function convertToRoman(num){
     }
     if (remainder > 0){
         // remove zeros
-        var removedZeros = remainder%10;
+        var removedZeros = remainder;
         while(removedZeros >= 10){
-            removedZeros = removedZeros%10;
+            removedZeros = Math.floor(removedZeros/10);
         }
-        // alert(removedZeros);
+        alert(removedZeros);
         // letters out to result
         if (removedZeros == 9 || removedZeros == 3){
             
             if (removedZeros == 9){
-                console.log(romanLetters[ii]+romanLetters[ii-2]);
-                result.push(romanLetters[ii]+romanLetters[ii-2])
+                console.log("nine: " + romanLetters[ii]+romanLetters[ii-2]);
+                result.push(romanLetters[ii]+romanLetters[ii-2]);
+                newNum = num - 9*divider[ii+1];
             }
             if (removedZeros == 3){
-                console.log(romanLetters[ii - 1]+romanLetters[ii-2]);
-                result.push(romanLetters[ii - 1]+romanLetters[ii-2]);
+                console.log("four: " + romanLetters[ii-1]+romanLetters[ii-2]);
+                result.push(romanLetters[ii-1]+romanLetters[ii-2]);
+                newNum = num - 4*divider[ii-1];
             }
         }else{
             for (var jj =0; jj < removedZeros; jj++){
                 console.log("testing");
                 console.log(romanLetters[ii-1]);
-                result.push(romanLetters[ii-1])
+                result.push(romanLetters[ii-1]);
+                newNum = num - maxRemainder*divider[ii-1];
             }    
         } 
     }
@@ -52,11 +59,11 @@ function convertToRoman(num){
     } else{ // remove the first digit in number
         // alert("recurse");
         // alert(remainder + " rZ" + removedZeros + " ii"+(ii-1) +"+ div" + divider[ii-1]);
-        var newNum = num - removedZeros*divider[ii-1];
+        console.log("newNum: " + newNum);
         if (newNum > 0){
             convertToRoman(newNum);
         }
     }
 }
-convertToRoman(49);
+convertToRoman(649);
 console.log(result.join(''));
