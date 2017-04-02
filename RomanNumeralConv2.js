c2d31b745408e8dcf5a82573fe29cc5edc3cf105
Recelis <jackymlui@live.com>
2017-04-02 17:22:31 +1000

    solved RomanNumberConverter2

--- RomanNumeralConv2.js
+++ RomanNumeralConv2.js
@@ -1,2 +1,49 @@
-var divider = [1,5,10,50,100,500,1000];
-var romanLetters = ["I","V", "X", "L","C", "D","M"];
+// roman numeral converter
+var romanLetters = {
+1:["I","V", "X"],
+2:["X","L", "C"],
+3:["C", "D", "M"]
+};
+
+var combinations = {    
+1:[0],
+2:[0,0],
+3:[0,0,0],
+4:[0,1],
+5:[1],
+6:[1,0],
+7:[1,0,0],
+8:[1,0,0,0],
+9:[0,2]
+};
+
+function convertToRoman(num) {
+// find divider 
+var digit = 1;
+var result = "";
+var newNum = num;
+while(newNum > 0){
+    var partResult = "";
+    var combObject = newNum%10;
+    newNum = Math.floor(newNum/10);
+    if (combObject == 0) {
+        digit ++; 
+        continue;
+    }
+    console.log("newNum " + newNum);
+    console.log("combObject " + combObject);
+    // loop over combinations
+    var combMatrix = combinations[combObject];
+    console.log("combMatrix " + combMatrix);
+    for (var ii =0; ii < combMatrix.length; ii++){
+        partResult+=romanLetters[digit][combMatrix[ii]];
+        console.log(romanLetters[digit][combMatrix[ii]]);
+    }
+    result = partResult + result;
+    console.log(result);
+    digit++;
+}
+return result;
+}
+
+convertToRoman(500)
