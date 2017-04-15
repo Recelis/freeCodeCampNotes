46d33b5d3d4fa3b0d4346c3c747b22915c9fe49e
JackyRecelis <jackymlui@live.com>
2017-04-15 13:01:48 +1000

    sym.js complete

--- sym.js
+++ sym.js
@@ -11,21 +11,64 @@ use array reduce
 reduce -> call function over all values of arrays
 function -> reduce -> call another function(incoming value, all values)
 if no match -> return 
-add all values to output array
+add all values to output array // rewrite using reduce
 
+// actual
 
+for loop of arguments
+    for loop through first array
+        remove repeating values first array
+        for loop through nth array
+            remove repeating values second
+            if match    
+                remove all matching values from both arrays
+                combine two arrays into one output
+sort output array
 */
-function sym(args) {
-    for (var ii =0; ii < args.length; ii++){
-            var output = args.reduce(function(acc, value,index, array) {
-            var checkAgainst = args.reduce(function(acc1, value1,index1, array1){
 
-            });// inefficient
-        }, 0);
+function filter(array, nonSym){
+    while(array.indexOf(nonSym) != -1){
+        console.log("ns " + nonSym);
+        index = array.indexOf(nonSym);    
+        array.splice(index, 1);	
+        console.log(index);
+        console.log("arr " + array);
     }
-    
-
-  return output;
+    return array;    
 }
 
-sym([1, 2, 3], [5, 2, 1, 4]);
+function checkMatch(output, symArray0, symArray1){
+    var symArrayCopy = Array.slice(symArray0);
+    for (var jj =0; jj < symArrayCopy.length; jj++){ // loop through output array values 
+        symArray0 = symArray0.slice(0,jj+1).concat(filter(symArray0.slice(jj+1), symArray0[jj])); // remove repeating values
+        for (var kk =0; kk < symArray1.length; kk++){ // symArrays dynamically change size in loop
+            symArray1 = symArray1.slice(0,kk+1).concat(filter(symArray1.slice(kk+1), symArray1[kk])); // remove repeating values
+            if (symArrayCopy[jj] === symArray1[kk]) {
+                var nonSym = symArray1[kk];
+                console.log("nsO " + nonSym);
+                // remove values in symArray0 and symArray1 
+                symArray0 = filter(symArray0, nonSym);
+                symArray1 = filter(symArray1, nonSym);
+                break; 
+            }
+        }   
+    }
+    output = symArray0.concat(symArray1);
+    return output;
+}
+
+function sym(args) {
+    var output = arguments[0];
+    for (var ii =1; ii < arguments.length; ii++){
+        console.log(output);
+        console.log(arguments[ii]);
+        var symArray0 = output;
+        var symArray1 = arguments[ii];
+        output = checkMatch(output,symArray0, symArray1);
+    }
+    output = output.sort(function(a, b){return a - b;});
+    return output;
+}
+
+var output = sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5]);
+console.log("final " + output);
