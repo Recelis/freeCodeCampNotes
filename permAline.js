2b70545b4ac18b49fbadb3cbe2ceeef110249a74
JackyRecelis <jackymlui@live.com>
2017-04-24 14:15:58 +1000

    finally solved this supertough permAline.js recursive problem!

--- permAline.js
+++ permAline.js
@@ -8,64 +8,79 @@ output: number
 
 do: 
 use recursion to try to count up all of the possible methods
+nodes = [[letter,0],[letter,0],[letter,0],[letter,0],etc]
 combinations(nodes, currentNode, count)
-    base case if nodes == [] return count++
+    base case: if all numbers in nodes is not zero, return count++
     else
-        nextNode = nodes.shift 
-        if nextNode == currentNode
-            return count
-        else
-            currentNode = nextNode
-            combinations(nodes, currentNode, count)
-            return count
+        while there are still zeros in nodes -> loop through using count 
+        if number in node[loopcount, number] is 0:
+          check if same as currentNode
+          if so: set number to -1
+          else: 
+            set number to 1
+            call combinations recursively with new nodes array and new currentNode
 */ 
-var levels = 0;
-function combinations(nodes, currentNode, count){
-    console.log("start level:" + levels);
-    if (nodes.length === 1) {
-        count++;
-        console.log("found one! count is:" + count);
-        return count;
+
+
+var noZeros = -1;
+
+function checkAvailableNodes(availableNodes){
+    for(var ii =0 ; ii < availableNodes.length; ii++){
+        if (availableNodes[ii][1] == 0) return ii;
+    }
+    return noZeros; // no 0s detected
+}
+
+function deepCopy(availableNodes){
+    var copied = [];
+    for (var ii =0; ii < availableNodes.length; ii++){
+        copied[ii] =  availableNodes[ii].slice();
+    }
+    return copied;
+}
+
+function zeroNodes(availableNodes){
+    var zeroedAvailableNodes = deepCopy(availableNodes);
+    for (var ii =0; ii < zeroedAvailableNodes.length; ii++){
+        zeroedAvailableNodes[ii][1] = 0;
+    }
+    return zeroedAvailableNodes;
+}
+
+function combinations(availableNodes, currentNode, perms){
+    if (checkAvailableNodes(availableNodes) == noZeros) { // base case, node length is one and not equal to currentNode
+        perms++;
+        return perms;
     }
     else{
-        var loopCount = 0;
-        var nodesMinusCurrent = nodes.slice();
-        while(loopCount < nodesMinusCurrent.length){ // want to keep looping to test out all other combinations
-            currentNode++;
-            console.log("");
-            console.log("newLoop: node " + nodes + " level:" + levels + " loopcount:" + loopCount+ " currentNode:" +currentNode); 
-            var nextNode = currentNode+1;
-            if (nextNode>= nodesMinusCurrent.length) nextNode = 0;
-            var nodeLoop =  1;
-            while (nodesMinusCurrent[nextNode] === nodesMinusCurrent[currentNode] && nodeLoop < nodes.length -1) { // if same, spin nodes
-                nextNode++;
-                nodeLoop++;
-                nodesMinusCurrent.splice(nextNode,1);
-                if (nextNode>= nodesMinusCurrent.length) nextNode = 0;
-                console.log("shift nextNode: " + nextNode); 
-            } 
-            
-            nodesMinusCurrent.splice(currentNode, 1);
-            console.log("nodesMinusCurrent_length:" + nodesMinusCurrent.length + " nodesMinusCurrent:" + nodesMinusCurrent +" count:" + count+ " currentNode:" +currentNode +" loopcount:" + loopCount + " levels:" + levels);
-            levels++;
-            if (currentNode < nextNode) nextNode--;
-            console.log("nextNode:" + nextNode);
-            count = combinations(nodesMinusCurrent, nextNode-1, count);
-            levels--;     
-            loopCount++;
-        } 
-        console.log("end of level:" + levels + " count:" + count + " node:" + nodes);
-        return count;
-        
+        var checkIndex = 0;
+        while(checkAvailableNodes(availableNodes) != noZeros){
+            if (availableNodes[checkIndex][1] == 0){
+                if (availableNodes[checkIndex][0] == currentNode) availableNodes[checkIndex][1] = -1;
+                else{
+                    availableNodes[checkIndex][1] = 1;
+                    
+                    var zeroedAvailableNodes = zeroNodes(availableNodes);
+                    zeroedAvailableNodes.splice(checkIndex,1);
+                    perms = combinations(zeroedAvailableNodes,availableNodes[checkIndex][0], perms);
+                }
+            }
+            checkIndex++;
+        }   
+        return perms;        
     }
 }
 
 function permAlone(str) {
-  var count = 0;  
+  var perms = 0;  
   var nodes = str.split("");
-  count = combinations(nodes, -1, count);
-  console.log(count);
-  return count;
+  var availableNodes = [];
+  for (var ii =0; ii < nodes.length; ii++){
+      availableNodes.push([nodes[ii], 0]);
+  } 
+  perms = combinations(availableNodes, -1, perms);
+  console.log(perms);
+  return perms;
 }
 
-permAlone('aabb');
+permAlone('abfdefa');
