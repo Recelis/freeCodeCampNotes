f73205f148e846a64073b3d4349635e44b3396f5
JackyRecelis <jackymlui@live.com>
2017-04-24 14:23:16 +1000

    finally solved this supertough permAline.js recursive problem!

--- permAline.js
+++ permAline.js
@@ -2,6 +2,10 @@
 permAline.js
 Jacky Lui
 
+Problem:
+Return the number of total permutations of the provided string that don't have repeated consecutive letters. Assume that all characters in the provided string are each unique.
+For example, aab should return 2 because it has 6 total permutations (aab, aab, aba, aba, baa, baa), but only 2 of them (aba and aba) don't have the same letter (in this case a) repeating.
+
 input: a bunch of letters
 do: calculate number of permutations that don't have consecutive repeating letters
 output: number
