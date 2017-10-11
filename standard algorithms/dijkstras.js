0a13ec41feb62d43062ec788c4fce8e218230212
Jacky <jackymlui@live.com>
2017-10-11 11:23:15 +1000

    setting up dijstras problem

---
+++ standard algorithms/dijkstras.js
@@ -0,0 +1,19 @@
+// graph
+
+var graph = {
+    Start: [{ A: 5 }, { B: 2 }],
+    A: [{ C: 4 }, { D: 2 }],
+    B: [{ A: 8 }, { D: 7 }],
+    C: [{ D: 6 }, { Finish: 3 }],
+    D: [{ Finish: 1 }]
+};
+
+function dikstras() {
+    var costs = {
+
+    };
+
+    var parents = {
+
+    };
+}
