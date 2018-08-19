739a2ef9b7c41fa146b63d48178777f42d54eb84
Jacky Lui <jackymlui@live.com>
2018-08-19 17:49:27 +1000

    more functional programming notes

---
+++ freecodecamp challenges/exerciseModelTracker.js
@@ -0,0 +1,15 @@
+// practising object oriented data structure code
+
+console.log("hello world!");
+
+function Exercise(){};
+
+Exercise.prototype = {
+    constructor:Exercise,
+    reps:0,
+    date:"",
+}
+
+
+
+
