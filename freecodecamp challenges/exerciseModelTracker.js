34fc5ca2c4b66f9d33e0e515e26f96281d009f03
Jacky Lui <jackymlui@live.com>
2018-08-20 22:18:35 +1000

    exercise tracker includes Run object

--- freecodecamp challenges/exerciseModelTracker.js
+++ freecodecamp challenges/exerciseModelTracker.js
@@ -1,14 +1,50 @@
 // practising object oriented data structure code
 
-console.log("hello world!");
-
-function Exercise(){};
-
-Exercise.prototype = {
+function Exercise(){}; // constructor
+let currentDate = new Date();
+let dateString = currentDate.toLocaleDateString();
+Exercise.prototype = { // prototype object
     constructor:Exercise,
-    reps:0,
-    date:"",
+    date:dateString,
+    type:"Endurance", // Strength, Explosion
 }
+
+// Run Objects for storage
+function Run(goalTime){
+    let time = goalTime; 
+    this.getTime = function(){
+        return time;
+    }
+};
+
+Run.prototype = Object.create(Exercise.prototype);
+
+Run.prototype.constructor = Run;
+
+let running = new Run(15);
+
+
+console.log("running time for " + running.date + " is:" + running.getTime());
+
+
+function ArmShoulderExercise(){};
+
+ArmShoulderExercise.prototype = Object.create(Exercise.prototype);
+
+ArmShoulderExercise.prototype = {
+    constructor:ArmShoulderExercise,
+}
+
+ArmShoulderExercise.prototype.constructor = ArmShoulderExercise;
+
+function LegExercise(){};
+
+LegExercise.prototype = Object.create(Exercise.prototype);
+
+LegExercise.prototype.constructor = LegExercise;
+
+let exercise = new LegExercise();
+console.log(exercise.date);
 
 
 
