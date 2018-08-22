c4193e959048c36a00d1c8eef7ae5fcc0dc293d9
Jacky Lui <jackymlui@live.com>
2018-08-22 22:03:03 +1000

    playing around with object oriented exercise plan

--- freecodecamp challenges/exerciseModelTracker.js
+++ freecodecamp challenges/exerciseModelTracker.js
@@ -10,10 +10,14 @@ Exercise.prototype = { // prototype object
 }
 
 // Run Objects for storage
-function Run(goalTime){
-    let time = goalTime; 
-    this.getTime = function(){
-        return time;
+function Run(goal, result){
+    let goalTime = goal;
+    let resultTime = result;
+    this.getGoalTime = function(){
+        return goalTime;
+    }
+    this.getResultTime = function(){
+        return resultTime;
     }
 };
 
@@ -21,27 +25,48 @@ Run.prototype = Object.create(Exercise.prototype);
 
 Run.prototype.constructor = Run;
 
-let running = new Run(15);
+let running = new Run(15, 22);
 
-
-console.log("running time for " + running.date + " is:" + running.getTime());
-
+console.log("Difference between expected and actual running time is " + (running.getResultTime() - running.getGoalTime())  + " minutes");
 
 function ArmShoulderExercise(){};
 
 ArmShoulderExercise.prototype = Object.create(Exercise.prototype);
 
-ArmShoulderExercise.prototype = {
-    constructor:ArmShoulderExercise,
-}
-
 ArmShoulderExercise.prototype.constructor = ArmShoulderExercise;
+
+ArmShoulderExercise.prototype.reps = "none";
+ArmShoulderExercise.prototype.weight = 0;
+
+let bicepCurls = new ArmShoulderExercise();
+let armRaises = new ArmShoulderExercise();
+let lawnmowerPulls = new ArmShoulderExercise();
+let reverseBicepCurl = new ArmShoulderExercise();
+let rows = new ArmShoulderExercise();
+let forearmsInner = new ArmShoulderExercise();
+let forearmsOuter = new ArmShoulderExercise();
+
+bicepCurls.reps = 30;
+armRaises.reps = 30;
+
+bicepCurls.type = "Strength";
+armRaises.type = "Endurance";
+lawnmowerPulls.type = "Strength";
+reverseBicepCurl.type = "Endurance";
+rows.type = "Endurance";
+forearmsInner.type = "Endurance";
+forearmsOuter.type = "Endurance";
+
+console.log(bicepCurls.type);
+console.log(armRaises.reps);
 
 function LegExercise(){};
 
 LegExercise.prototype = Object.create(Exercise.prototype);
 
 LegExercise.prototype.constructor = LegExercise;
+
+
 
 let exercise = new LegExercise();
 console.log(exercise.date);
