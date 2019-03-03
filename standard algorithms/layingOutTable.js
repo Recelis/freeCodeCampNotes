c2932938bf8bb49956336738d34ca4b8fc6a2ff1
JackyRecelis <jackymlui@live.com>
2019-03-03 13:25:55 +1000

    heading created

--- standard algorithms/layingOutTable.js
+++ standard algorithms/layingOutTable.js
@@ -1,4 +1,5 @@
 let tableContents = [
+    ["name", "height", "country"],
     ["Killimanjaro", 5895, "Tanzania"],
     ["Everest", 8848, "Nepal"],
     ["Mount Fuji", 3776, "Japan"],
@@ -8,13 +9,20 @@ let tableContents = [
     ["Popocatepetl", 5465, "Mexico"]
 ];
 
-let TableDraw = function myTable(table){
+let TableDraw = function myTable(table) {
     this.table = table;
+    this.padding = function(length, character) { //this is a priveliged method
+        let generate = "";
+        for (let ii = 0; ii < length; ii++) {
+            generate += character;
+        }
+        return generate;
+    }
 };
 
-// for each column
-TableDraw.prototype.columnWidth = function(column){
-    let longestRow = this.table.reduce(function(prev, curr){
+// for each column (public methods)
+TableDraw.prototype.columnWidth = function (column) {
+    let longestRow = this.table.reduce(function (prev, curr) {
         let sPrev = prev;
         let sCurr = curr;
         if (typeof prev[column] !== "string") sPrev[column] = prev[column].toString();
@@ -24,13 +32,20 @@ TableDraw.prototype.columnWidth = function(column){
     console.log(longestRow);
     return longestRow[column].length;
 }
-TableDraw.prototype.draw = function(table){
+
+TableDraw.prototype.createHeaders = function (nameSpaces, heightSpaces, countrySpaces) {
+    // add spaces in front of height and country for space between column
+    console.log("name" + this.padding(nameSpaces - 4, " ") + " height" + this.padding(heightSpaces - 6, " ") + " " + this.padding(countrySpaces - 7, " ") + "country");
+    console.log(this.padding(nameSpaces, "_") + " " + this.padding(heightSpaces, "_") + " " + this.padding(countrySpaces, "_"));
+}
+
+TableDraw.prototype.draw = function (table) {
 }
 
 // every object will have same copy
 // have to use function keyword instead of arrow because arrow
 // functions create a new instance of the function which means that this doesn't work (I believe)
-TableDraw.prototype.doSomething = function(word){
+TableDraw.prototype.doSomething = function (word) {
     // returns array of length height containing strings of length height, one cell.
     console.log("doing something and print " + word);
 }
@@ -40,9 +55,10 @@ let newTable = new TableDraw(tableContents);
 let maxWidth = [];
 let columns = 3;
 
-for (let ii =0; ii < columns; ii ++){
+for (let ii = 0; ii < columns; ii++) {
     maxWidth.push(newTable.columnWidth(ii));
 }
 console.log(maxWidth);
-newTable.doSomething("word");    
+newTable.createHeaders(...maxWidth);
+newTable.doSomething("word");
 
