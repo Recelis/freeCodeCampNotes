6335729d14bcc673fbfe731a9280aa3aee12522f
JackyRecelis <jackymlui@live.com>
2019-03-04 20:13:01 +1000

    completed a minus function

--- standard algorithms/vector.js
+++ standard algorithms/vector.js
@@ -9,8 +9,16 @@ Vector.prototype.plus = function(vector2){
     return new Vector(x2, y2);
 }
 
+Vector.prototype.minus = function(vector2){
+    x2 = this.x - vector2.x;
+    y2 = this.y - vector2.y;
+    return new Vector(x2, y2);
+}
+
 
 let vector1 = new Vector(1,1);
 let vector2 = new Vector(2,2);
 let vector3 = vector1.plus(vector2);
+console.log(vector3.x + " " + vector3.y);
+vector3 = vector1.minus(vector2);
 console.log(vector3.x + " " + vector3.y);
